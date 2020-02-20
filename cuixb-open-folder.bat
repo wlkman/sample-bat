19f20cc37aecb0af9e7cf85049d36fe83c960789
wlkman <cuixinbin@yeah.net>
2020-02-20 16:44:03 +0800

    Update cuixb-open-folder.bat

--- cuixb-open-folder.bat
+++ cuixb-open-folder.bat
@@ -1,18 +1,17 @@
 @ECHO OFF
 TITLE find folder
-start "" "C:\Users\XinBinCui\Desktop\Pending Task.xlsx"
 :start
 echo ###########################folder list##############################
 echo 1--m2
 echo 2--esmtc
 echo 3--e-solution
-echo 4--WSE platform
+echo 4--pending task
 echo 5--Micro service Platform
 set/p in= please choose folder you want to open:
 if "%in%"=="1" start C:\Users\XinBinCui\.m2
 if "%in%"=="2" start C:\Users\XinBinCui\CUIXINBIN\GitHub
 if "%in%"=="3" start C:\Users\XinBinCui\Desktop\esolution
-if "%in%"=="4" start "" "C:\Users\XinBinCui\Desktop\WSE platform"
+if "%in%"=="4" start "" "C:\Users\XinBinCui\Desktop\Pending Task.xlsx"
 if "%in%"=="5" start "" "C:\Users\XinBinCui\Desktop\Micro service Platform"
 goto :start
 PAUSE
