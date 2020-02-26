22881f1625b4c0c8ea3627a57a18609b21915796
wlkman <cuixinbin@yeah.net>
2020-02-26 13:40:32 +0800

    Update cuixb-open-folder.bat

--- cuixb-open-folder.bat
+++ cuixb-open-folder.bat
@@ -6,12 +6,12 @@ echo 1--m2
 echo 2--esmtc
 echo 3--e-solution
 echo 4--pending task
-echo 5--Micro service Platform
+echo 5--Golang
 set/p in= please choose folder you want to open:
 if "%in%"=="1" start C:\Users\XinBinCui\.m2
 if "%in%"=="2" start C:\Users\XinBinCui\CUIXINBIN\GitHub
 if "%in%"=="3" start C:\Users\XinBinCui\Desktop\esolution
 if "%in%"=="4" start "" "C:\Users\XinBinCui\Desktop\Pending Task.xlsx"
-if "%in%"=="5" start "" "C:\Users\XinBinCui\Desktop\Micro service Platform"
+if "%in%"=="5" start C:\Users\XinBinCui\Desktop\Micro service Platform\GolangWorkSpace\GolangProject
 goto :start
 PAUSE
